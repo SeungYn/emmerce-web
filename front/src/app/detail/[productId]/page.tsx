5348ea14d42304d80c7cf483ab1158dc6d3fa6f3
SeungYn <kwls0407@gmail.com>
2023-10-18 23:22:41 +0900

    feat: DetailMain 컴포넌트 레이아웃 작성

--- front/src/app/detail/[productId]/page.tsx
+++ front/src/app/detail/[productId]/page.tsx
@@ -1,11 +1,19 @@
 import MaxXLContainer from '@/components/common/container/MaxXLContainer';
 import Location from '@/components/common/location/Location';
+import ProductDetailInfo from '@/components/product/detail/ProductDetailInfo/ProductDetailInfo';
+import ProductDetailMain from '@/components/product/detail/ProductDetailMain/ProductDetailMain';
 
 export default function page() {
   return (
     <>
       <MaxXLContainer className='mt-4'>
         <Location />
+      </MaxXLContainer>
+      <MaxXLContainer className='mt-4'>
+        <ProductDetailMain />
+      </MaxXLContainer>
+      <MaxXLContainer className='mt-4'>
+        <ProductDetailInfo />
       </MaxXLContainer>
     </>
   );
---
+++ front/src/components/product/detail/ProductDetailInfo/ProductDetailInfo.tsx
@@ -0,0 +1,23 @@
+export default function ProductDetailInfo() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href='#'>상품 상세정보</a>
+        </li>
+
+        <li>
+          <a href='#'>고객리뷰(265)</a>
+        </li>
+
+        <li>
+          <a href='#'>상품 Q&A(23)</a>
+        </li>
+
+        <li>
+          <a href='#'>배송/반품/교환</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
