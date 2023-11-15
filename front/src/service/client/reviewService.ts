0daf266e0db7f2c86c77e5c5bf6e05ed8da5fe07
SeungYn <kwls0407@gmail.com>
2023-11-16 02:36:47 +0900

    feat: reviewService 추가

---
+++ front/src/service/client/reviewService.ts
@@ -0,0 +1,23 @@
+import { AxiosInstance } from 'axios';
+import { GetReviewsRes } from '../types/review';
+
+export default class ReviewService {
+  constructor(private axios: AxiosInstance) {}
+
+  async getReviews(productId: number | string): Promise<GetReviewsRes>;
+
+  async getReviews(productId: number | string, page?: number, size?: number) {
+    let query = `/product/${productId}/reviews`;
+    if (page && size) query += `?page=${page}&size=${size}`;
+
+    const { data } = await this.axios.get<GetReviewsRes>(query);
+    return data;
+  }
+
+  // async getReviews(productId: number | string) {
+  //   const { data } = await this.axios.get<GetReviewsRes>(
+  //     `/product/${productId}/reviews`
+  //   );
+  //   return data;
+  // }
+}
---
+++ front/src/service/types/review.d.ts
@@ -0,0 +1,18 @@
+export type Review = {
+  reviewId: number;
+  title: string;
+  description: string;
+  starScore: number;
+  reviewImgList: string[];
+  memberId: number;
+  writer: string;
+};
+
+export type GetReviewsRes = {
+  content: Review[];
+  pageNumber: number;
+  totalPages: number;
+  totalElements: number;
+  first: boolean;
+  last: boolean;
+};
