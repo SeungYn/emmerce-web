32d9393533dccf242f5a69ad43ebce927d362089
SeungYn <kwls0407@gmail.com>
2023-11-20 14:34:39 +0900

    feat: cart client service 클래스 작성

---
+++ front/src/service/client/cartService.ts
@@ -0,0 +1,33 @@
+import { AxiosInstance } from 'axios';
+
+export default class CartService {
+  constructor(private axios: AxiosInstance) {}
+
+  async add({ productId, quantity }: { productId: number; quantity: number }) {
+    const query = `/cart/product`;
+    const { data } = await this.axios.post(query, {
+      productId,
+      quantity,
+    });
+    console.log('add res', data);
+    return data;
+  }
+
+  async deleteByProductId(productId: number) {
+    const query = `/cart/product/${productId}`;
+    const { data } = await this.axios.delete(query);
+    return data;
+  }
+
+  async getCartItemList() {
+    const query = `/cart/product`;
+    const { data } = await this.axios.get(query);
+    return data;
+  }
+
+  async clear() {
+    const query = `/cart/clear`;
+    const { data } = await this.axios.delete(query);
+    return data;
+  }
+}
--- front/src/service/client/index.ts
+++ front/src/service/client/index.ts
@@ -1,5 +1,6 @@
 import AuthService from './authService';
 import { instance } from './axiosInstance';
+import CartService from './cartService';
 import ProductService from './productService';
 import ReviewService from './reviewService';
 
@@ -7,6 +8,7 @@ const service = {
   auth: new AuthService(instance),
   review: new ReviewService(instance),
   product: new ProductService(instance),
+  cart: new CartService(instance),
 };
 
 export default service;
