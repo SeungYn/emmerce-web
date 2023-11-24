202aecaf80364bf5c593ae825b2cf9069e5820e1
SeungYn <kwls0407@gmail.com>
2023-11-24 23:47:12 +0900

    feat: reactquery 기반 장바구니 hook 작성

--- front/src/hooks/api/cart/useCart.ts
+++ front/src/hooks/api/cart/useCart.ts
@@ -1,6 +1,11 @@
 import service from '@/service/client';
-import { CartAddReq } from '@/service/types/cart';
-import { useMutation, useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import { CartAddReq, CheckCartItem } from '@/service/types/cart';
+import {
+  useMutation,
+  useQuery,
+  useQueryClient,
+  useSuspenseQuery,
+} from '@tanstack/react-query';
 
 export function useCartItemList() {
   const res = useQuery({
@@ -14,6 +19,7 @@ export function useCartItemListSuspense() {
   const res = useSuspenseQuery({
     queryKey: ['cart'],
     queryFn: () => service.cart.getCartItemList(),
+    refetchOnMount: true,
   });
   return res;
 }
@@ -24,8 +30,7 @@ export function useCartMutations() {
   });
 
   const deleteMutate = useMutation({
-    mutationFn: (productId: number) =>
-      service.cart.deleteByProductId(productId),
+    onMutate: () => {},
   });
 
   const clearMutate = useMutation({
@@ -44,4 +49,148 @@ export function useCartAdd(cb: () => void) {
     },
   });
   return addMutate;
+}
+
+export function useCartDeleteItem() {
+  const queryClient = useQueryClient();
+
+  const deleteMutate = useMutation({
+    mutationFn: (checkCartItem: CheckCartItem) =>
+      service.cart.deleteByProductId(checkCartItem),
+    onMutate: async (checkCartItem) => {
+      await queryClient.cancelQueries({ queryKey: ['cart'] });
+      const previousList = queryClient.getQueryData<CheckCartItem[]>(['cart'])!;
+
+      queryClient.setQueryData(['cart'], () => {
+        const result = previousList.filter(
+          (item) => item.cartProductId !== checkCartItem.cartProductId
+        );
+        return result;
+      });
+      return { previousList, checkCartItem };
+    },
+    onError: (err, checkCartItem, context) => {
+      queryClient.setQueryData(['cart'], context?.previousList);
+    },
+  });
+  return deleteMutate;
+}
+
+export function useCheckAllCartItems() {
+  const queryClient = useQueryClient();
+  const mutate = useMutation({
+    onMutate: () => {
+      const previousList = queryClient.getQueryData<CheckCartItem[]>(['cart'])!;
+
+      queryClient.setQueryData(['cart'], () => {
+        const result = previousList.map((item) => ({
+          ...item,
+          isCheck: true,
+        }));
+        return result;
+      });
+      return { previousList };
+    },
+  });
+
+  return mutate;
+}
+
+export function useUnCheckAllCartItems() {
+  const queryClient = useQueryClient();
+  const mutate = useMutation({
+    onMutate: () => {
+      const previousList = queryClient.getQueryData<CheckCartItem[]>(['cart'])!;
+
+      queryClient.setQueryData(['cart'], () => {
+        const result = previousList.map((item) => ({
+          ...item,
+          isCheck: false,
+        }));
+        return result;
+      });
+      return { previousList };
+    },
+  });
+
+  return mutate;
+}
+
+export function useToggleCheckCartItem() {
+  const queryClient = useQueryClient();
+  const mutate = useMutation({
+    onMutate: (checkCartItem: CheckCartItem) => {
+      const previousList = queryClient.getQueryData<CheckCartItem[]>(['cart'])!;
+
+      queryClient.setQueryData(['cart'], () => {
+        const result = previousList.map((item) => {
+          if (item.cartProductId === checkCartItem.cartProductId)
+            return { ...item, isCheck: !item.isCheck };
+          return { ...item };
+        });
+
+        return result;
+      });
+      return { previousList, checkCartItem };
+    },
+  });
+
+  return mutate;
+}
+
+export function useUpCountCartItem() {
+  const queryClient = useQueryClient();
+  const mutate = useMutation({
+    onMutate: (checkCartItem: CheckCartItem) => {
+      const previousList = queryClient.getQueryData<CheckCartItem[]>(['cart'])!;
+
+      queryClient.setQueryData(['cart'], () => {
+        const result = previousList.map((item) => {
+          if (item.cartProductId === checkCartItem.cartProductId) {
+            return { ...item, totalCount: checkCartItem.totalCount + 1 };
+          }
+          return { ...item };
+        });
+
+        return result;
+      });
+      return { previousList, checkCartItem };
+    },
+  });
+
+  return mutate;
+}
+
+export function useDownCountCartItem() {
+  const queryClient = useQueryClient();
+  const mutate = useMutation({
+    onMutate: (checkCartItem: CheckCartItem) => {
+      const previousList = queryClient.getQueryData<CheckCartItem[]>(['cart'])!;
+
+      queryClient.setQueryData(['cart'], () => {
+        const result = previousList.map((item) => {
+          if (item.cartProductId === checkCartItem.cartProductId) {
+            const countResult = checkCartItem.totalCount - 1;
+
+            return { ...item, totalCount: countResult < 1 ? 1 : countResult };
+          }
+          return { ...item };
+        });
+
+        return result;
+      });
+      return { previousList, checkCartItem };
+    },
+  });
+
+  return mutate;
+}
+
+export function useClearCart() {
+  const queryClient = useQueryClient();
+  const mutate = useMutation({
+    mutationFn: () => service.cart.clear(),
+  });
+
+  return mutate;
 }
