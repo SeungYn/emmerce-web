f1d987a873b2771cd71058ef758e119d31fde00b
SeungYn <kwls0407@gmail.com>
2023-11-26 01:31:56 +0900

    add: payment 아이콘 및 taliwind config 수정

--- front/src/components/order/Payment/PaymentForm.tsx
+++ front/src/components/order/Payment/PaymentForm.tsx
@@ -1,8 +1,12 @@
 export default function PaymentForm() {
   return (
-    <div>
+    <div className='my-8'>
       <div>
         <h3 className='text-xl font-bold mb-4'>결제 수단</h3>
+      </div>
+
+      <div>
+        <img src='/assets/icons/kakao-pay.png' />
       </div>
     </div>
   );
