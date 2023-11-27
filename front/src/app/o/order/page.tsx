a446066542d899bde73b9f45535b3a831942a262
SeungYn <kwls0407@gmail.com>
2023-11-27 13:12:21 +0900

    feat: 다음Post 스크립트, 타입 추가

--- front/src/app/o/order/page.tsx
+++ front/src/app/o/order/page.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import CartHeader from '@/components/cart/common/CartHeader/CartHeader';
 import OrderList from '@/components/order/OrderList/OrderList';
 import PaymentDelivery from '@/components/order/PaymentDelivery/PaymentDelivery';
@@ -5,6 +7,9 @@ import PaymentDelivery from '@/components/order/PaymentDelivery/PaymentDelivery'
 export default function page() {
   return (
     <>
+      {/* 다음 주소 api */}
+      <Script src='https://developers.kakao.com/sdk/js/kakao.js' async />
+
       <CartHeader currentPage='order' />
       <OrderList />
       <PaymentDelivery />
