5eb2733e2545cbafa4c2c588565afa90b40bf497
SeungYn <kwls0407@gmail.com>
2023-11-29 14:57:51 +0900

    feat: kakaoPay Store 작성

---
+++ front/src/store/payment/kakaoPayStore.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+
+interface KakaoPayPopUp {
+  redirect_pc_url: string;
+  setRedirect_pc_url: (nextUrl: string) => void;
+}
+
+const initial = {
+  redirect_pc_url: '',
+};
+
+export const useKakaoPayStore = create<KakaoPayPopUp>()((set) => ({
+  ...initial,
+  setRedirect_pc_url: (nextUrl) =>
+    set((state) => ({ ...state, redirect_pc_url: nextUrl })),
+  reset: () => set({ ...initial }),
+}));
