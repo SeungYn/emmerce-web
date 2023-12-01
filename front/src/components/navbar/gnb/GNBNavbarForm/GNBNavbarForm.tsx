f83843c83d340158e8c6c9001075c9f7a5102f9e
SeungYn <kwls0407@gmail.com>
2023-12-02 00:54:53 +0900

    [#2] design: GNB 검색창 레이아웃

---
+++ front/src/components/navbar/gnb/GNBNavbarForm/GNBNavbarForm.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useState } from 'react';
+import { SlMagnifier } from 'react-icons/sl';
+
+export default function GNBNavbarForm() {
+  const [search, setSearch] = useState<string>('');
+  return (
+    <form className='flex items-center w-[600px] border-b border-black absolute left-[50%] translate-x-[-50%]'>
+      <input
+        type='text'
+        placeholder='메이플 지작 스칸다 60% 할인 판매중'
+        className='text-base flex-grow  py-2'
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
+      <button>
+        <SlMagnifier className='text-xl' />
+      </button>
+    </form>
+  );
+}
