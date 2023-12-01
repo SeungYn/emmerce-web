8946f38b44b9d42a4c7fbd134ac7688d64df7318
SeungYn <kwls0407@gmail.com>
2023-12-02 00:55:46 +0900

    [#2] design: 새로운 GNB 추가

--- front/src/components/navbar/gnb/GNB/GNB.tsx
+++ front/src/components/navbar/gnb/GNB/GNB.tsx
@@ -2,6 +2,9 @@ import Category from '../../category/Category/Category';
 import { CategoryList } from '@/service/types/category';
 import GNBLogo from '../GNBLogo/GNBLogo';
 import NavbarForm from '../../NavbarForm/NavbarForm';
+import GNBMenu from '../GNBMenu/GNBMenu';
+import MaxXLContainer from '@/components/common/container/MaxXLContainer';
+import GNBNavbarForm from '../GNBNavbarForm/GNBNavbarForm';
 
 type Props = {
   categoryList: CategoryList;
@@ -9,11 +12,15 @@ type Props = {
 
 export default function GNB({ categoryList }: Props) {
   return (
-    <nav className='w-full' suppressHydrationWarning>
-      <div className='flex justify-between'>
-        <GNBLogo />
-        <NavbarForm />
-        <div>3</div>
+    <nav className='w-full pt-2' suppressHydrationWarning>
+      <div className='border-b border-zinc-200 py-3'>
+        <MaxXLContainer>
+          <div className='flex justify-between items-center  relative'>
+            <GNBLogo />
+            <GNBNavbarForm />
+            <GNBMenu />
+          </div>
+        </MaxXLContainer>
       </div>
       <Category categoryList={categoryList} />
     </nav>
