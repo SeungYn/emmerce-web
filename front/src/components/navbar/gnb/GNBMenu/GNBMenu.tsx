d87e65ed00face550190d19f52debe2d85f35138
SeungYn <kwls0407@gmail.com>
2023-12-02 01:02:25 +0900

    [#2] feat: GNB 메뉴 장바구니 클릭 이벤트  추가

--- front/src/components/navbar/gnb/GNBMenu/GNBMenu.tsx
+++ front/src/components/navbar/gnb/GNBMenu/GNBMenu.tsx
@@ -1,25 +1,48 @@
+'use client';
 import { FaRegHeart } from 'react-icons/fa';
 import { IoPersonOutline } from 'react-icons/io5';
 import { BsCart } from 'react-icons/bs';
 import { CiDeliveryTruck } from 'react-icons/ci';
 import GNBLogin from '../GNBLogin/GNBLogin';
+import { useUserContext } from '@/context/auth/UserContext';
+import { useRouter } from 'next/navigation';
+import { useAuthFormContext } from '@/context/auth/AuthFormContext';
 
 export default function GNBMenu() {
+  const { userInfo } = useUserContext();
+  const { handleOpen } = useAuthFormContext();
+  const router = useRouter();
+  const onMoveCartPage = () => {
+    if (userInfo?.token) {
+      router.push('/o/cart');
+    } else {
+      handleOpen();
+    }
+  };
+
   return (
     <div className='flex flex-col'>
       <GNBLogin />
       <ul className='flex text-2xl items-center gap-2'>
         <li>
-          <FaRegHeart />
+          <button className='flex items-center'>
+            <FaRegHeart />
+          </button>
         </li>
         <li>
-          <IoPersonOutline />
+          <button className='flex items-center'>
+            <IoPersonOutline />
+          </button>
         </li>
         <li>
-          <CiDeliveryTruck size='32px' />
+          <button className='flex items-center'>
+            <CiDeliveryTruck size='32px' />
+          </button>
         </li>
         <li>
-          <BsCart />
+          <button className='flex items-center' onClick={onMoveCartPage}>
+            <BsCart />
+          </button>
         </li>
       </ul>
     </div>
