57fadf8a2528d185200d1d09798afcb1a8a49df7
SeungYn <kwls0407@gmail.com>
2023-12-04 17:04:16 +0900

    feat: GNB 검색 폼 라우터 기능,  GNB dynamic import 추가 및 CSS 수정

--- front/src/components/navbar/gnb/GNBNavbarForm/GNBNavbarForm.tsx
+++ front/src/components/navbar/gnb/GNBNavbarForm/GNBNavbarForm.tsx
@@ -1,12 +1,23 @@
 'use client';
 
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { FormEvent, useState } from 'react';
 import { SlMagnifier } from 'react-icons/sl';
 
 export default function GNBNavbarForm() {
   const [search, setSearch] = useState<string>('');
+  const router = useRouter();
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push('/search?keyword=' + search);
+  };
+
   return (
-    <form className='flex items-center w-[600px] border-b border-black absolute left-[50%] translate-x-[-50%]'>
+    <form
+      className='flex items-center w-[600px] border-b border-black absolute left-[50%] translate-x-[-50%]'
+      onSubmit={onSubmit}
+    >
       <input
         type='text'
         placeholder='메이플 지작 스칸다 60% 할인 판매중'
