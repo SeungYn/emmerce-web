5586a02de018477b41fcac50dd87e8e4b2b21895
SeungYn <kwls0407@gmail.com>
2023-12-04 16:47:05 +0900

    [#4] feat: 검색 페이지

--- front/src/app/search/page.tsx
+++ front/src/app/search/page.tsx
@@ -2,25 +2,35 @@ import MaxXLContainer from '@/components/common/container/MaxXLContainer';
 import MainItemLink from '@/components/common/listitem/MainItemLink/MainItemLink';
 import Location from '@/components/common/location/Location';
 import GNB from '@/components/navbar/gnb/GNB/GNB';
+import ProductListPagination from '@/components/product/common/ProductListPagination/ProductListPagination';
 import FilterForm from '@/components/product/filter/FilterForm/FilterForm';
+import ListManipulation from '@/components/product/filter/ListManipulation/ListManipulation';
+import { serverService } from '@/service/server';
 import { getCategoryList } from '@/service/server/category';
-import { getProductListByCategory } from '@/service/server/product';
-import { searchItem } from '@/util/mock/data/item';
-import Link from 'next/link';
-import { MdKeyboardArrowRight } from 'react-icons/md';
+
+//export const dynamic = 'force-dynamic';
 
 type Props = {
-  params: { id: number };
+  searchParams: {
+    keyword: string;
+    brand: string;
+    limit: string;
+    minPrice: string;
+    maxPrice: string;
+  };
 };
 
-export default async function page() {
-  const data = searchItem;
+export default async function page({
+  searchParams: { keyword, brand, limit, minPrice, maxPrice },
+}: Props) {
+  const data = await serverService.product.getProductListByKeyword({
+    keyword,
+    brand,
+    limit,
+    minPrice,
+    maxPrice,
+  });
   const categoryList = await getCategoryList();
-  const currentBlock = Math.floor(data.pageNumber / 10) + 1;
-  const pageArr = Array.from(
-    { length: 10 >= data.totalPages ? data.totalPages : 10 },
-    (_, i) => i + currentBlock
-  );
 
   return (
     <>
@@ -30,29 +40,21 @@ export default async function page() {
       <MaxXLContainer className='mt-4'>
         <Location />
       </MaxXLContainer>
+      {/* search mention */}
+      <MaxXLContainer className='mt-6'>
+        <h2 className='text-xl'>
+          <span className=' font-bold'>{`'${keyword}'`}</span>에 대한
+          검색결과입니다.
+        </h2>
+      </MaxXLContainer>
 
       <MaxXLContainer className='mt-4'>
         <div className='flex gap-10'>
-          <FilterForm />
+          <div className='basis-[20%]'>
+            <FilterForm keyword={keyword} productList={data.content} />
+          </div>
           <div className='w-full'>
-            <div className='flex justify-between text-sm text-gray-500 border-b border-gray-300 pb-5'>
-              <ul className='flex gap-8'>
-                <li>낮은가격순</li>
-                <li>낮은가격순</li>
-                <li>낮은가격순</li>
-                <li>낮은가격순</li>
-                <li>낮은가격순</li>
-                <li>낮은가격순</li>
-              </ul>
-              <select>
-                <option value=''>10개씩보기</option>
-                <option value=''>20개씩보기</option>
-                <option value=''>30개씩보기</option>
-                <option value=''>40개씩보기</option>
-                <option value=''>50개씩보기</option>
-                <option value=''>60개씩보기</option>
-              </select>
-            </div>
+            <ListManipulation />
             <div className='mt-4 mx-3'>
               <ul className='w-full flex flex-shrink-0 flex-wrap mt-4 gap-4 '>
                 {data.content.map((item, i) => (
@@ -66,33 +68,13 @@ export default async function page() {
               </ul>
             </div>
             <div className='flex justify-center mt-4'>
-              <ul className='flex gap-4'>
-                {!data.first && (
-                  <>
-                    <li>
-                      <Link href=''>이이전</Link>
-                    </li>
-                    <li>
-                      <Link href=''>이전</Link>
-                    </li>
-                  </>
-                )}
-                {pageArr.map((v) => (
-                  <li key={v}>
-                    <Link href=''>{v}</Link>
-                  </li>
-                ))}
-                {!data.last && (
-                  <>
-                    <li>
-                      <Link href=''>다음</Link>
-                    </li>
-                    <li>
-                      <Link href=''>다다음</Link>
-                    </li>
-                  </>
-                )}
-              </ul>
+              <ProductListPagination
+                pageNumber={data.pageNumber}
+                totalPages={data.totalPages}
+                first={data.first}
+                last={data.last}
+                totalElements={data.totalElements}
+              />
             </div>
           </div>
         </div>
