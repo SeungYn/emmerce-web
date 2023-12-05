e38643c2da9653718840b4e4323a400b50da0ff0
SeungYn <kwls0407@gmail.com>
2023-12-05 15:53:08 +0900

    fix[#6]: FinalPaymentInfoByCartContainer 컴포넌트 데이터 패칭 적용

--- front/src/container/order/FinalPaymentInfoContainer.tsx
+++ front/src/container/order/FinalPaymentInfoContainer.tsx
@@ -1,17 +1,28 @@
 'use client';
 
 import FinalPaymentInfo from '@/components/order/FinalPaymentInfo/FinalPaymentInfo';
+import { useCartItemList } from '@/hooks/api/cart/useCart';
 import { usePostOrder } from '@/hooks/api/order/useOrder';
+import {
+  discountCartListPrice,
+  finalCartListPrice,
+  originCartListPrice,
+} from '@/util/lib/cart';
 
-export default function FinalPaymentInfoContainer() {
+export default function FinalPaymentInfoByCartContainer() {
+  const { data: cartItemList } = useCartItemList();
   const { postOrderMutate } = usePostOrder();
+
+  const discountPrice = discountCartListPrice(cartItemList);
+  const discountedPrice = finalCartListPrice(cartItemList);
+  const originPrice = originCartListPrice(cartItemList);
 
   return (
     <FinalPaymentInfo
-      deliverPrice={10000}
-      discountPrice={10000}
-      discoutedPrice={10000}
-      originPrice={10000}
+      deliverPrice={0}
+      discountPrice={discountPrice}
+      discountedPrice={discountedPrice}
+      originPrice={originPrice}
       handlePostOrder={postOrderMutate.mutate}
     />
   );
