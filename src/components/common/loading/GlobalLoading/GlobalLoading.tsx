2fc62860101810e942472261630f3674ac7f46c4
SeungYn <kwls0407@gmail.com>
2023-12-07 23:55:46 +0900

    add[#14]: 글로벌 로딩 컴포넌트 작성

---
+++ src/components/common/loading/GlobalLoading/GlobalLoading.tsx
@@ -0,0 +1,19 @@
+'use client';
+import useNavigationLoadingEvent from '@/hooks/common/useNavigationLoadingEvent';
+import Image from 'next/image';
+
+export default function GlobalLoading() {
+  const isLoading = useNavigationLoadingEvent();
+
+  if (!isLoading) return null;
+  return (
+    <div className='fixed bottom-0 left-0 z-[9999]'>
+      <Image
+        src='/assets/icons/double-ring.svg'
+        width={70}
+        height={70}
+        alt='로딩이미지'
+      />
+    </div>
+  );
+}
