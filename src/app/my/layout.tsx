2f59f01c3b2ce347bebde9d74db22b0058d1bf87
SeungYn <kwls0407@gmail.com>
2023-12-12 15:50:15 +0900

    feat[#16]: 리뷰 모달 추가

--- src/app/my/layout.tsx
+++ src/app/my/layout.tsx
@@ -2,8 +2,6 @@ import MaxXLContainer from '@/components/common/container/MaxXLContainer';
 import MyNavigation from '@/components/my/MyNavigation/MyNavigation';
 import GNB from '@/components/navbar/gnb/GNB/GNB';
 import { serverService } from '@/service/server';
-import Link from 'next/link';
-import { HTMLAttributes } from 'react';
 
 export default async function layout({
   children,
@@ -21,12 +19,9 @@ export default async function layout({
       <MaxXLContainer>
         <section className='flex gap-4 mt-8'>
           <MyNavigation />
-
           <section className='flex-shrink-0 flex-grow'>{children}</section>
         </section>
       </MaxXLContainer>
     </>
   );
 }
-
-const CTListHeader = '';
