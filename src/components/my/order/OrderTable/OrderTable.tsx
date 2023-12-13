2e4a1eb2d3b149ce9ce7993286942b039a7c5af4
SeungYn <kwls0407@gmail.com>
2023-12-13 15:33:43 +0900

    refactor[#18]: 주문내역 후기 작성 상태 추가

--- src/components/my/order/OrderTable/OrderTable.tsx
+++ src/components/my/order/OrderTable/OrderTable.tsx
@@ -1,5 +1,6 @@
-import { OrderHistory, OrderHistoryItem } from '@/service/types/order';
-import { PropsWithChildren, ReactNode } from 'react';
+import ProductReviewModalContainer from '@/container/my/review/ProductReviewModalContainer/ProductReviewModalContainer';
+import { OrderHistory } from '@/service/types/order';
+import { PropsWithChildren, useState } from 'react';
 
 export default function OrderTable({ children }: PropsWithChildren) {
   return <table className='w-full'>{children}</table>;
@@ -29,7 +30,8 @@ function Header() {
 
 type BodyProps = OrderHistory;
 
-function Body({ orderProductRespList, orderDate }: BodyProps) {
+function Body({ orderProductRespList, orderDate, orderId }: BodyProps) {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <tbody>
       <tr className='bg-gray-100 block px-4 py-2 mt-2'>
@@ -44,6 +46,7 @@ function Body({ orderProductRespList, orderDate }: BodyProps) {
         const {
           name,
           discountPrice,
+          productId,
           brand,
           originalPrice,
           quantity,
@@ -51,56 +54,67 @@ function Body({ orderProductRespList, orderDate }: BodyProps) {
           reviewStatus,
         } = item;
         return (
-          <>
-            <tr
-              className='grid border-b border-gray-300'
-              style={{
-                gridTemplateColumns: '5fr  2fr 1fr 2fr 2fr 2fr',
-                gridAutoRows: '133px',
-                alignItems: 'center',
-                justifyItems: 'center',
-              }}
-            >
-              <td className='flex shrink-0 justify-self-start gap-4 p-2'>
-                <img
-                  src={titleImg}
-                  alt='카트 상품 이미지'
-                  width={85}
-                  className='w-[85px] aspect-square'
-                />
-                <div className='flex-grow'>
-                  <p className='text-gray-500'>{brand}</p>
-                  <p className='font-blod'>{name}</p>
-                  <p>옵션</p>
-                </div>
-              </td>
+          <tr
+            className='grid border-b border-gray-300'
+            style={{
+              gridTemplateColumns: '5fr  2fr 1fr 2fr 2fr 2fr',
+              gridAutoRows: '133px',
+              alignItems: 'center',
+              justifyItems: 'center',
+            }}
+            key={productId + orderDate.toString()}
+          >
+            <td className='flex shrink-0 justify-self-start gap-4 p-2'>
+              {/* eslint-disable @next/next/no-img-element */}
+              <img
+                src={titleImg}
+                alt='카트 상품 이미지'
+                width={85}
+                height={85}
+                className='w-[85px] aspect-square'
+              />
+              <div className='flex-grow'>
+                <p className='text-gray-500'>{brand}</p>
+                <p className='font-blod'>{name}</p>
+                <p>옵션</p>
+              </div>
+            </td>
 
-              <td className='p-2'>
-                {(originalPrice * quantity).toLocaleString()}원
-              </td>
-              <td className='flex items-center gap-2 p-2'>
-                <div>{quantity}개</div>
-              </td>
-              <td className='p-2'>
-                {' '}
-                {((originalPrice - discountPrice) * quantity).toLocaleString()}
-                원
-              </td>
-              <td className='p-2'>
-                {(discountPrice * quantity).toLocaleString()}원
-              </td>
-              <td className='p-2 text-sm'>
-                <p className='text-center'>무료배송</p>
-                {!reviewStatus ? (
-                  <button className='bg-black text-white py-1 px-4'>
+            <td className='p-2'>
+              {(originalPrice * quantity).toLocaleString()}원
+            </td>
+            <td className='flex items-center gap-2 p-2'>
+              <div>{quantity}개</div>
+            </td>
+            <td className='p-2'>
+              {' '}
+              {((originalPrice - discountPrice) * quantity).toLocaleString()}원
+            </td>
+            <td className='p-2'>
+              {(discountPrice * quantity).toLocaleString()}원
+            </td>
+            <td className='p-2 text-sm'>
+              <p className='text-center'>무료배송</p>
+              {!reviewStatus ? (
+                <>
+                  <button
+                    className='bg-black text-white py-1 px-4'
+                    onClick={() => setOpenModal(true)}
+                  >
                     후기작성
                   </button>
-                ) : (
-                  <p>후기작성완료</p>
-                )}
-              </td>
-            </tr>
-          </>
+                  <ProductReviewModalContainer
+                    openModal={openModal}
+                    setOpenModal={setOpenModal}
+                    orderHistoryItem={item}
+                    orderId={orderId as number}
+                  />
+                </>
+              ) : (
+                <p>후기작성완료</p>
+              )}
+            </td>
+          </tr>
         );
       })}
     </tbody>
