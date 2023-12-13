421f2bc97cc49f0d3687f217c793b371ce26dbb3
SeungYn <kwls0407@gmail.com>
2023-12-13 15:29:32 +0900

    chore[#18]: target -> es6 변경 및 config 수정

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  devIndicators: {
+    buildActivityPosition: 'bottom-left',
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'emmerce.duckdns.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'emmerce-bucket.s3.ap-northeast-2.amazonaws.com',
       },
     ],
   },
