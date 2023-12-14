8b46255475b522d32082af298d0283c7ec327503
SeungYn <kwls0407@gmail.com>
2023-12-14 17:14:49 +0900

    feat[#9]: 에러 타입 추가

---
+++ src/service/types/error.d.ts
@@ -0,0 +1,14 @@
+export type ServerErrorRes = {
+  timestamp: string;
+  path: string;
+  status: number;
+  error: string;
+  requestId: string;
+  exception: string;
+  message: string;
+};
+
+export type GlobalErrorType = {
+  message: string;
+  status: number;
+};
