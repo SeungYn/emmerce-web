44513e54dc79e00eeac002a899c20ad8ce72c45e
SeungYn <kwls0407@gmail.com>
2023-12-28 01:20:50 +0900

    feat[#10]: 마이페이지 메타데이터 추가

--- src/app/my/layout.tsx
+++ src/app/my/layout.tsx
@@ -3,6 +3,11 @@ import GlobalFooter from '@/components/common/footer/GlobalFooter/GlobalFooter';
 import MyNavigation from '@/components/my/MyNavigation/MyNavigation';
 import GNB from '@/components/navbar/gnb/GNB/GNB';
 import { serverService } from '@/service/server';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '마이페이지',
+};
 
 export default async function layout({
   children,
