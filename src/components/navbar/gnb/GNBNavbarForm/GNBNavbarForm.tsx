5b9408e3f3c1a4281e5c5a9d38a8f5f812b46990
SeungYn <kwls0407@gmail.com>
2023-12-30 01:40:24 +0900

    refactor[#19]: form에서 최근검색창 제어 및 기타 기능 추가

--- src/components/navbar/gnb/GNBNavbarForm/GNBNavbarForm.tsx
+++ src/components/navbar/gnb/GNBNavbarForm/GNBNavbarForm.tsx
@@ -1,21 +1,33 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import { FormEvent, useState } from 'react';
 import { SlMagnifier } from 'react-icons/sl';
+import GNBRecentSearch from '../GNBRecentSearch/GNBRecentSearch';
+import { useRecentSearchStoreAction } from '@/store/common/recentSearchStore';
+import useRefOutClickHelper from '@/hooks/common/useRefOutClickHelper';
+import useCustomRouter from '@/hooks/common/useCustomRouter';
 
 export default function GNBNavbarForm() {
   const [search, setSearch] = useState<string>('');
-  const router = useRouter();
+  const [openRecentSearch, setOpenRecentSearch] = useState(false);
+  const { ref } = useRefOutClickHelper<HTMLFormElement>({
+    setOpenState: setOpenRecentSearch,
+  });
+  const { addItem } = useRecentSearchStoreAction();
+  const router = useCustomRouter();
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (search === '') return;
+    addItem(search);
+    setOpenRecentSearch(false);
     router.push('/search?keyword=' + search);
   };
 
   return (
     <form
-      className='flex items-center w-[600px] border-b border-black absolute left-[50%] translate-x-[-50%]'
+      ref={ref}
+      className='flex items-center w-[600px] border-b border-black absolute left-[50%] translate-x-[-50%] z-10'
       onSubmit={onSubmit}
     >
       <input
@@ -23,11 +35,17 @@ export default function GNBNavbarForm() {
         placeholder='검색어를 입력해 주세요.'
         className='text-base flex-grow  py-2'
         onChange={(e) => setSearch(e.target.value)}
+        onFocus={(e) => {
+          setOpenRecentSearch(true);
+        }}
         value={search}
       />
       <button>
         <SlMagnifier className='text-xl' />
       </button>
+      {openRecentSearch && (
+        <GNBRecentSearch setOpenState={setOpenRecentSearch} />
+      )}
     </form>
   );
 }
