3a9c989eab0f4ca7e3c5b986d1d67a427dabaa8c
SeungYn <kwls0407@gmail.com>
2024-01-11 22:32:24 +0900

    fix:[#27] ShowcaseItems 아이템 MainItemLink로 변경 및 css 수정

--- src/components/home/content/CustomItems/CustomItems.tsx
+++ src/components/home/content/CustomItems/CustomItems.tsx
@@ -1,4 +1,6 @@
-import MiniItem from '@/components/common/listitem/MiniItem/MiniItem';
+import MiniItem, {
+  MiniItemWrapper,
+} from '@/components/common/listitem/MiniItem/MiniItem';
 import Header from '../Header/Header';
 import { Product } from '@/service/types/product';
 
@@ -11,37 +13,16 @@ export default function CustomItems({ productList }: Props) {
     <div>
       <Header
         title='고객님을 위해 준비했어요'
-        subTitle='당싱을 위한 맞춤 상품'
+        subTitle='당신을 위한 맞춤 상품'
       />
 
       <div className='overflow-auto mt-4'>
         <ul className='flex flex-wrap shrink-0 w-[1500px] gap-4'>
           {productList.map((item) => (
-            <li key={item.productId} className='w-[340px]'>
-              <MiniItem product={item} />
-            </li>
+            <MiniItemWrapper key={item.productId} width={340} height={110}>
+              <MiniItemWrapper.Item product={item} />
+            </MiniItemWrapper>
           ))}
-          {/* <li className='w-[340px]'>
-            <MiniItem />
-          </li>
-          <li className='w-[340px]'>
-            <MiniItem />
-          </li>
-          <li className='w-[340px]'>
-            <MiniItem />
-          </li>
-          <li className='w-[340px]'>
-            <MiniItem />
-          </li>
-          <li className='w-[340px]'>
-            <MiniItem />
-          </li>
-          <li className='w-[340px]'>
-            <MiniItem />
-          </li>
-          <li className='w-[340px]'>
-            <MiniItem />
-          </li> */}
         </ul>
       </div>
     </div>
