c58c1d2ad6ba5e5c9b939810ff93cea64a4829b6
SeungYn <kwls0407@gmail.com>
2024-01-11 21:52:37 +0900

    fix[#27]: 메인페이지 MiniItem 컴포넌트 css 수정 및 wrapper 추가

--- src/components/common/listitem/MiniItem/MiniItem.tsx
+++ src/components/common/listitem/MiniItem/MiniItem.tsx
@@ -1,13 +1,33 @@
 import { Product } from '@/service/types/product';
 import Image from 'next/image';
-import Link from 'next/link';
 import CustomGlobalLoadingLink from '../../customlink/CustomGlobalLoadingLink/CustomGlobalLoadingLink';
+import { PropsWithChildren } from 'react';
 
-type Props = {
+type MiniItemWrapperProps = {
+  width: number;
+  height: number;
+  className?: string;
+};
+
+export function MiniItemWrapper({
+  children,
+  width,
+  height,
+  className,
+}: PropsWithChildren<MiniItemWrapperProps>) {
+  return (
+    <li className={`${className}`} style={{ width, height }}>
+      {children}
+    </li>
+  );
+}
+MiniItemWrapper.Item = MiniItem;
+
+type MiniItemProps = {
   product: Product;
 };
 
-export default function MiniItem({ product }: Props) {
+export default function MiniItem({ product }: MiniItemProps) {
   const {
     productId,
     name,
@@ -22,25 +42,19 @@ export default function MiniItem({ product }: Props) {
     <CustomGlobalLoadingLink href={`/detail/${productId}`}>
       <div className='flex gap-2 shrink-0 '>
         {/* after없이 border을 지정하면 미세 틈이 생기는데 after로 border을 해주면 커버 가능 */}
-        <div className='relative shrink-0 rounded-xl w-[110px] overflow-hidden after:content-[""] after:absolute after:inset-0 after:border after:border-gray-400 after:rounded-xl'>
+        <div className='relative shrink-0 grow-0 rounded-xl w-[110px] h-[110px] overflow-hidden after:content-[""] after:absolute after:inset-0 after:border after:border-gray-300 after:rounded-xl'>
           {/* 이미지 태그가 */}
-          <img
-            src={titleImg}
-            width={111}
-            height={111}
-            style={{ width: '111px', height: '111px' }}
-            alt='상품 이미지'
-          />
+          <Image src={titleImg} fill alt='상품 이미지' />
         </div>
 
         <div className='flex flex-col justify-center grow gap-1'>
           <h4 className='text-base font-bold'>{brand}</h4>
-          <p className='text-sm text-gray-500 break-all line-clamp-3'>{name}</p>
+          <p className='text-sm text-gray-500 break-all line-clamp-1'>{name}</p>
           <span className='line-through text-gray-300 text-sm'>
-            {originalPrice}원
+            {originalPrice.toLocaleString()}원
           </span>
           <div className='flex justify-between text-xl font-semibold'>
-            <p>{discountPrice}원</p>
+            <p>{discountPrice.toLocaleString()}원</p>
             <span className='text-red-500'>{discountRate}%</span>
           </div>
         </div>
