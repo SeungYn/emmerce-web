2b9e8ce37c0e99ca7f92cd55c330f7b1af464375
SeungYn <kwls0407@gmail.com>
2024-01-11 22:31:40 +0900

    fix:[#27] RankingItems 아이템 MainItemLink로 변경 및 css 수정

--- src/components/home/content/RankingItems/RankingItems.tsx
+++ src/components/home/content/RankingItems/RankingItems.tsx
@@ -1,29 +1,29 @@
-'use client';
-
 import Header from '../Header/Header';
-import MainItem from '@/components/common/listitem/MainItem/MainItem';
 import { Product } from '@/service/types/product';
-import useMovePageToDetail from '@/hooks/product/useMovePageToDetail';
+import MainItemLink from '@/components/common/listitem/MainItemLink/MainItemLink';
 
 type Props = {
   productList: Product[];
 };
 
 export default function RankingItems({ productList }: Props) {
-  const handleMovePage = useMovePageToDetail();
-
   return (
     <div>
       <Header title='랭킹' subTitle='가장 많이 판매된 인기상품' />
 
       <div className='mt-4'>
-        <ul className='w-full flex flex-shrink-0 flex-wrap justify-between mt-4'>
+        <ul className='w-full flex flex-shrink-0 flex-wrap  mt-4'>
           {productList.map((item, i) => (
-            <li key={item.productId} className='[&:nth-child(n+6)]:mt-10'>
-              <MainItem
+            <li
+              key={item.productId}
+              className='[&:nth-child(n+6)]:mt-10 w-[256px] h-[340px] px-[5px]'
+            >
+              <MainItemLink
                 item={item}
                 rank={i + 1}
-                handleMovePage={handleMovePage}
+                targetLink={`/detail/${item.productId}`}
+                width={246}
+                height={240}
               />
             </li>
           ))}
