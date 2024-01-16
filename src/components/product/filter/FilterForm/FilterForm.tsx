261298c04caae4434def7434f636fbba46f77d0c
SeungYn <kwls0407@gmail.com>
2024-01-16 12:17:02 +0900

    refactor[#29]: 상품리스트 FilterForm 최적화

--- src/components/product/filter/FilterForm/FilterForm.tsx
+++ src/components/product/filter/FilterForm/FilterForm.tsx
@@ -1,12 +1,11 @@
 'use client';
-import { SlMagnifier } from 'react-icons/sl';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import FIlterFormAccordion from '@/components/common/accordion/FIlterFormAccordion';
 import { categoryRex } from '@/components/common/location/Location';
 import NextURLSearchParams from '@/util/lib/urlSearchParams';
 import { Product } from '@/service/types/product';
-import { filterBrandList } from '@/util/lib/util';
-import useCustomRouter from '@/hooks/common/useCustomRouter';
+import PriceDoubleSliderContainer from '@/container/product/filter/PriceDoubleSliderContainer';
+import BrandFilterContainer from '@/container/product/filter/BrandFilterContainer';
 
 type Props = {
   keyword?: string;
@@ -14,9 +13,7 @@ type Props = {
 };
 
 export default function FilterForm({ keyword, productList }: Props) {
-  const router = useCustomRouter();
   const searchParams = new NextURLSearchParams(useSearchParams());
-  const pathname = usePathname();
   const mainCateStr = searchParams
     .getQueryString('mainCate')
     ?.match(categoryRex);
@@ -30,10 +27,6 @@ export default function FilterForm({ keyword, productList }: Props) {
     : mainCateStr
     ? mainCateStr[1]
     : '';
-
-  const pushRoute = (pathname: string, query: string) => {
-    router.push(pathname + '?' + query);
-  };
 
   return (
     <form>
@@ -52,36 +45,10 @@ export default function FilterForm({ keyword, productList }: Props) {
       </div>
       <FIlterFormAccordion>
         <FIlterFormAccordion.Item title='브랜드'>
-          <div className='flex border-b border-black mt-4'>
-            <input
-              type='text'
-              className='w-full text-sm py-2 '
-              placeholder='브랜드를 검색하세요'
-            />
-            <SlMagnifier
-              className='text-2xl '
-              style={{ stroke: 'black', strokeWidth: '5' }}
-            />
-          </div>
-          <ul className='mt-4'>
-            {Array.from(filterBrandList(productList)).map((v) => (
-              <li key={v as string} className='flex gap-2 items-center'>
-                <label className='flex gap-2 items-center pl-2 text-sm text-gray-600 cursor-pointer'>
-                  <input
-                    type='radio'
-                    name='brand'
-                    onChange={(e) => {
-                      pushRoute(
-                        pathname,
-                        searchParams.setQueryString('brand', v as string)
-                      );
-                    }}
-                  />{' '}
-                  {v as string}
-                </label>
-              </li>
-            ))}
-          </ul>
+          <BrandFilterContainer productList={productList} />
+        </FIlterFormAccordion.Item>
+        <FIlterFormAccordion.Item title='가격'>
+          <PriceDoubleSliderContainer />
         </FIlterFormAccordion.Item>
       </FIlterFormAccordion>
     </form>
