237892469ecdb4b5ab7eb02c7feeeb5996a56004
SeungYn <kwls0407@gmail.com>
2024-01-17 15:17:53 +0900

    feat[#30]: 줌 기능 완성

--- src/components/product/detail/DetailZoom/DetailZoom.tsx
+++ src/components/product/detail/DetailZoom/DetailZoom.tsx
@@ -7,12 +7,12 @@ type Props = {
   titleImg: string;
 };
 
-const imageSize = 640;
-const initialContainerCoordnate = {
+const INITIAL_IMAGE_SIZE = { width: 640, height: 640 };
+const INITIAL_CONTAINER_COORDNATE = {
   left: 0,
   top: 0,
 };
-const initialMaginifierCoordnate = {
+const INITIAL_MAGINIFIER_COORDNATE = {
   left: 0,
   top: 0,
   width: 200,
@@ -22,15 +22,16 @@ const initialMaginifierCoordnate = {
 export default function DetailZoom({ titleImg }: Props) {
   const containerRef = useRef<HTMLDivElement>(null);
   const magnifierRef = useRef<HTMLDivElement>(null);
+  const [isEnter, setIsEnter] = useState(false);
   const [containerCoordnate, setContainerCoordnate] = useState(
-    initialContainerCoordnate
+    INITIAL_CONTAINER_COORDNATE
   );
   const [maginifierCoordnate, setMaginifierCoordnate] = useState(
-    initialMaginifierCoordnate
+    INITIAL_MAGINIFIER_COORDNATE
   );
   const ratioMagnifierZoomTarget = getRatioMaginifier(
     maginifierCoordnate.width,
-    imageSize
+    INITIAL_IMAGE_SIZE.width
   );
 
   // container 최초 좌표
@@ -39,34 +40,43 @@ export default function DetailZoom({ titleImg }: Props) {
       const { x, y } = containerRef.current.getBoundingClientRect();
       setContainerCoordnate({ left: x, top: y });
     }
+    // document.addEventListener('mousemove', (e) => {
+    //   console.log(e.clientY);
+    // });
   }, []);
 
   return (
     <div
       ref={containerRef}
-      className='relative'
+      className='relative cursor-crosshair'
+      onMouseEnter={() => setIsEnter(true)}
+      onMouseLeave={() => setIsEnter(false)}
       onWheel={(e) => {
         let scale = maginifierCoordnate.scale + e.deltaY * -0.01;
-        let sizeDifference = 1;
-        if (e.deltaY < 0) sizeDifference *= -1;
         scale = Math.min(Math.max(1, scale), 2);
-        sizeDifference *=
-          scale * maginifierCoordnate.width - maginifierCoordnate.width;
+        let sizeDifference = 1;
+
+        // 휠 다운에 따른 크기 전, 후의 차이
+        if (e.deltaY < 0) {
+          sizeDifference *= -1;
+          sizeDifference *=
+            scale * INITIAL_MAGINIFIER_COORDNATE.width -
+            maginifierCoordnate.width;
+        } else {
+          sizeDifference *=
+            maginifierCoordnate.width -
+            scale * INITIAL_MAGINIFIER_COORDNATE.width;
+        }
+
         if (scale === maginifierCoordnate.scale) sizeDifference = 0;
 
-        console.log(
-          sizeDifference,
-          scale,
-          imageSize,
-          scale * maginifierCoordnate.width - maginifierCoordnate.width,
-          maginifierCoordnate.width
-        );
         setMaginifierCoordnate((v) => ({
           ...v,
           scale,
-          width: scale * initialMaginifierCoordnate.width,
-          // left: v.left + sizeDifference / 2,
-          // top: v.top + sizeDifference / 2,
+          width: scale * INITIAL_MAGINIFIER_COORDNATE.width,
+          height: scale * INITIAL_MAGINIFIER_COORDNATE.height,
+          left: v.left + sizeDifference / 2,
+          top: v.top + sizeDifference / 2,
         }));
       }}
       onMouseMove={(e) => {
@@ -75,6 +85,7 @@ export default function DetailZoom({ titleImg }: Props) {
           y = 0;
         if (magnifierRef.current) {
           // const { x: cx, y: cy } = magnifierRef.current.getBoundingClientRect();
+          //console.log(containerCoordnate.top, e.clientY);
           x =
             e.clientX - containerCoordnate.left - maginifierCoordnate.width / 2;
           y =
@@ -82,18 +93,18 @@ export default function DetailZoom({ titleImg }: Props) {
 
           if (x < 0) x = 0;
           if (y < 0) y = 0;
-          if (x + maginifierCoordnate.width > imageSize)
-            x = imageSize - maginifierCoordnate.width;
-          if (y + maginifierCoordnate.height > imageSize)
-            y = imageSize - maginifierCoordnate.height;
+          if (x + maginifierCoordnate.width > INITIAL_IMAGE_SIZE.width)
+            x = INITIAL_IMAGE_SIZE.width - maginifierCoordnate.width;
+          if (y + maginifierCoordnate.height > INITIAL_IMAGE_SIZE.height)
+            y = INITIAL_IMAGE_SIZE.height - maginifierCoordnate.height;
           setMaginifierCoordnate((v) => ({ ...v, left: x, top: y }));
         }
       }}
     >
       <Image
         src={titleImg}
-        width={imageSize}
-        height={imageSize}
+        width={INITIAL_IMAGE_SIZE.width}
+        height={INITIAL_IMAGE_SIZE.height}
         style={{ width: '640px', height: '640px' }}
         sizes='640px'
         alt='아이템 이미지'
@@ -103,9 +114,9 @@ export default function DetailZoom({ titleImg }: Props) {
         className={`absolute  left-[660px] w-[640px] h-[640px] top-0 `}
         style={{
           backgroundImage: `url(${titleImg})`,
-          backgroundSize: `${imageSize * ratioMagnifierZoomTarget}px ${
-            imageSize * ratioMagnifierZoomTarget
-          }px`,
+          backgroundSize: `${
+            INITIAL_IMAGE_SIZE.width * ratioMagnifierZoomTarget
+          }px ${INITIAL_IMAGE_SIZE.height * ratioMagnifierZoomTarget}px`,
           backgroundPosition: `-${
             maginifierCoordnate.left * ratioMagnifierZoomTarget
           }px -${maginifierCoordnate.top * ratioMagnifierZoomTarget}px`,
@@ -121,8 +132,9 @@ export default function DetailZoom({ titleImg }: Props) {
           top: maginifierCoordnate.top,
           width: maginifierCoordnate.width,
           height: maginifierCoordnate.height,
+          opacity: isEnter ? 1 : 0,
         }}
-        className='top-0 absolute  bg-black '
+        className='top-0 absolute  bg-slate-100  bg-opacity-50 border border-black '
       ></div>
     </div>
   );
