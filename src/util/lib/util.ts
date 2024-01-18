0bdf46ed64c3817d27f92c8311af05514474e825
SeungYn <kwls0407@gmail.com>
2024-01-18 16:14:13 +0900

    fix[#30]: 상품 별점 변환 유틸 함수 로직 수정

--- src/util/lib/util.ts
+++ src/util/lib/util.ts
@@ -3,7 +3,7 @@ import { Product } from '@/service/types/product';
 
 // 별점 100점 환산 (측정값-1) * 100/(선택항목 수-1)
 export function translateStarScoreToPercent(score: number) {
-  return ((score - 1) / 4) * 100;
+  return score * 20;
 }
 
 export function combineCateNameAndId({
