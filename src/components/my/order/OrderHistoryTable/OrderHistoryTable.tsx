a7fc58c095bacf678c12f22f04eeac2946fdb9ea
SeungYn <kwls0407@gmail.com>
2024-01-24 00:04:56 +0900

    refactor[#32]: 주문내역 컴포넌트 이름 변경

--- src/components/my/order/OrderHistoryTable/OrderHistoryTable.tsx
+++ src/components/my/order/OrderHistoryTable/OrderHistoryTable.tsx
@@ -1,12 +1,14 @@
 import ProductReviewModalContainer from '@/container/my/review/ProductReviewModalContainer/ProductReviewModalContainer';
-import { OrderHistory } from '@/service/types/order';
+import { DeliveryStatus, OrderHistory } from '@/service/types/order';
+import { translateYYYYMMDD } from '@/util/lib/my/order';
+import Image from 'next/image';
 import { PropsWithChildren, useState } from 'react';
 
-export default function OrderTable({ children }: PropsWithChildren) {
+export default function OrderHistoryTable({ children }: PropsWithChildren) {
   return <table className='w-full'>{children}</table>;
 }
-OrderTable.Header = Header;
-OrderTable.Body = Body;
+OrderHistoryTable.Header = Header;
+OrderHistoryTable.Body = Body;
 
 function Header() {
   return (
@@ -14,7 +16,7 @@ function Header() {
       <tr
         className='grid bg-white py-3  [&>*]:text-center w-full border-t-2 border-t-black  border-b border-b-gray-300'
         style={{
-          gridTemplateColumns: '5fr  2fr 1fr 2fr 2fr 2fr',
+          gridTemplateColumns: '5fr 2fr 1fr 2fr 2fr 2fr',
         }}
       >
         <th>상품명/선택사항</th>
@@ -36,12 +38,9 @@ function Body({ orderProductRespList, orderDate, orderId }: BodyProps) {
     <tbody>
       <tr className='bg-gray-100 block px-4 py-2 mt-2'>
         <td className='bold text-gray-500'>
-          주문일 : {new Date(orderDate).toISOString()}
+          주문일 : {translateYYYYMMDD(new Date(orderDate))}
         </td>
       </tr>
-      {orderProductRespList.length === 0 && (
-        <div>주문내역이 존재하지 않습니다.</div>
-      )}
       {orderProductRespList.map((item) => {
         const {
           name,
@@ -52,13 +51,14 @@ function Body({ orderProductRespList, orderDate, orderId }: BodyProps) {
           quantity,
           titleImg,
           reviewStatus,
+          deliveryStatus,
         } = item;
         return (
           <tr
             className='grid border-b border-gray-300'
             style={{
               gridTemplateColumns: '5fr  2fr 1fr 2fr 2fr 2fr',
-              gridAutoRows: 'minmax(133px, 133px)',
+              gridAutoRows: 'minmax(130px, 133px)',
               alignItems: 'center',
               justifyItems: 'center',
             }}
@@ -66,12 +66,12 @@ function Body({ orderProductRespList, orderDate, orderId }: BodyProps) {
           >
             <td className='flex shrink-0 justify-self-start gap-4 p-2'>
               {/* eslint-disable @next/next/no-img-element */}
-              <img
+              <Image
                 src={titleImg}
                 alt='카트 상품 이미지'
                 width={85}
                 height={85}
-                className='w-[85px] aspect-square'
+                className='w-[85px] aspect-square shrink-0 grow-0'
               />
               <div className='flex-grow'>
                 <p className='text-gray-500'>{brand}</p>
@@ -94,22 +94,24 @@ function Body({ orderProductRespList, orderDate, orderId }: BodyProps) {
               {(discountPrice * quantity).toLocaleString()}원
             </td>
             <td className='p-2 text-sm'>
-              <p className='text-center'>무료배송</p>
+              <p className='text-center'>{getDeliveryStatus(deliveryStatus)}</p>
               {!reviewStatus ? (
-                <>
-                  <button
-                    className='bg-black text-white py-1 px-4'
-                    onClick={() => setOpenModal(true)}
-                  >
-                    후기작성
-                  </button>
-                  <ProductReviewModalContainer
-                    openModal={openModal}
-                    setOpenModal={setOpenModal}
-                    orderHistoryItem={item}
-                    orderId={orderId as number}
-                  />
-                </>
+                deliveryStatus === 'COMPLETE' ? (
+                  <>
+                    <button
+                      className='bg-black text-white py-1 px-4'
+                      onClick={() => setOpenModal(true)}
+                    >
+                      후기작성
+                    </button>
+                    <ProductReviewModalContainer
+                      openModal={openModal}
+                      setOpenModal={setOpenModal}
+                      orderHistoryItem={item}
+                      orderId={orderId as number}
+                    />
+                  </>
+                ) : null
               ) : (
                 <p>후기작성완료</p>
               )}
@@ -119,4 +121,19 @@ function Body({ orderProductRespList, orderDate, orderId }: BodyProps) {
       })}
     </tbody>
   );
+}
+
+function getDeliveryStatus(delivery: DeliveryStatus) {
+  switch (delivery) {
+    case 'CANCEL':
+      return '배송취소';
+    case 'READY':
+      return '준비중';
+    case 'COMPLETE':
+      return '배송완료';
+    case 'ING':
+      return '배송중';
+    default:
+      return '배송확인불가';
+  }
 }
