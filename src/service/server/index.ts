85d915a34d6bc6f64dd5210f9f1bf52fae1f4a12
SeungYn <kwls0407@gmail.com>
2024-01-24 00:00:23 +0900

    feat[#32]: myServerService 추가

--- src/service/server/index.ts
+++ src/service/server/index.ts
@@ -3,10 +3,12 @@ import { ProductServerService } from './product';
 import { CategoryServerService } from './category';
 import PaymentServerService from './payment';
 import OrderServerService from './order';
+import { MyServerService } from './my';
 
 export const serverService = {
   product: new ProductServerService(fetchInstance),
   category: new CategoryServerService(fetchInstance),
   payment: new PaymentServerService(fetchInstance),
   order: new OrderServerService(fetchInstance),
+  my: new MyServerService(fetchInstance),
 };
