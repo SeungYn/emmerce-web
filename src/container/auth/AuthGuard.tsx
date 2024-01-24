0749ecc69fcf4620c32a35389ffcfd81f6f1538c
SeungYn <kwls0407@gmail.com>
2024-01-24 21:21:16 +0900

    feat: 클라이언트 AuthGuard 컴포넌트 추가

---
+++ src/container/auth/AuthGuard.tsx
@@ -0,0 +1,13 @@
+import { useUserContext } from '@/context/auth/UserContext';
+import { useRouter } from 'next/router';
+import { PropsWithChildren } from 'react';
+
+export default function AuthGuard({ children }: PropsWithChildren) {
+  const { userInfo } = useUserContext();
+  const router = useRouter();
+  if (!userInfo) {
+    alert('로그인이 필요합니다..!');
+    router.back();
+  }
+  return <>{children}</>;
+}
