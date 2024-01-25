73a9745aec9e6ce53611b95fc76a5e3e57782d56
SeungYn <kwls0407@gmail.com>
2024-01-25 22:58:36 +0900

    feat: 토큰 쿠키 관련 상수 추가

--- src/util/constants/auth/index.ts
+++ src/util/constants/auth/index.ts
@@ -1,4 +1,10 @@
+// 토큰 이름
 export const TOKEN_NAME = {
   access: 'access-token',
   refesh: 'refresh-token',
+} as const;
+
+// cookie 기간
+export const COOKIE_OPTIONS = {
+  'max-age': 60 * 60 * 24 * 7,
 } as const;
