03ccffe34d42ea6e71373f552e3f0aeca459597c
SeungYn <kwls0407@gmail.com>
2024-02-01 16:31:48 +0900

    refactor[#34]: 폼 애니메이션 배경과 폼이 따로 나오도록 수정

--- src/components/auth/AuthBackground.tsx
+++ src/components/auth/AuthBackground.tsx
@@ -1,28 +1,35 @@
 'use client';
 
-import { motion } from 'framer-motion';
-import AuthForm from './AuthForm';
+import { Variants, motion } from 'framer-motion';
 import { useAuthFormContext } from '@/context/auth/AuthFormContext';
+import AuthFormContiner from '@/container/auth/AuthFormContiner';
+
+const MotionVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    transition: { delay: 0.3 },
+    transitionEnd: { display: 'none' },
+  },
+  visible: {
+    opacity: 1,
+    display: 'flex',
+  },
+};
 
 export default function AuthBackground() {
   const context = useAuthFormContext();
 
   return (
     <motion.section
+      layout
       style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
       className='top-0 z-50 fixed w-full h-[100vh] text-white flex justify-center items-center'
       initial={false}
       animate={context.isOpen ? 'visible' : 'hidden'}
-      variants={{
-        hidden: { opacity: 0, display: 'none' },
-        visible: {
-          opacity: 1,
-          display: 'flex',
-        },
-      }}
-      transition={{ duration: 0.5 }}
+      variants={MotionVariants}
+      transition={{ duration: 0.3 }}
     >
-      <AuthForm {...context} />
+      <AuthFormContiner {...context} />
     </motion.section>
   );
 }
